39eea2db7467bbe3fec41735602eb5a423bc20ad
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-10-26 09:32:41 +0300

    Added  a solution

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,53 @@
-const key = new Key();
+class Key {
+  private signature: number = Math.random();
 
-const house = new MyHouse(key);
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  constructor(
+    public key: Key,
+    public door: boolean = false,
+    public tenants: Person[] = []
+  ) {}
+
+  abstract openDoor(keyToCheck: Key): void;
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("The door is open");
+    } else {
+      console.log("The door is closed. Please use a key to open it.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(keyToCheck: Key): void {
+    if (this.key.getSignature() === keyToCheck.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
 const person = new Person(key);
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
